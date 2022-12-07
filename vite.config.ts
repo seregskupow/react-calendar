afb10583518e8b61e00b2ee2cebf3e01196c06d4
seregskupow <54269429+seregskupow@users.noreply.github.com>
2022-12-07 02:45:25 +0200

    config fixes

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,6 @@ export default defineConfig({
 	plugins: [react()],
 	base: process.env.NODE_ENV === 'production' ? '/react-calendar/' : '/',
 	resolve: {
-		alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+		alias: [{ find: '@', replacement: path.resolve(__dirname, './src') }],
 	},
 });
